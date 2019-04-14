58700068c39716af1b47be2bc5c929144ebffc47
Vittorio Guerrera <vittorio.mcgill@gmail.com>
2019-04-14 13:10:41 -0400

    Created renderer, texture, and initial pixel buffer.

--- src/SDL test.cpp
+++ src/SDL test.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <cstring>
 using namespace std;
-
 
 /*
  * Installing SDL :
@@ -23,36 +23,73 @@ int WinMain() {
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 600;
 
-	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL Init Failed" << endl;
 		return 1;
 	}
 
+	// create SDL window
 	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-	if(window == NULL){
+	if (window == NULL) {
 		cout << "Could not create window." << SDL_GetError() << endl;
 		SDL_Quit();
 		return 2;
 	}
 
+	// create renderer and texture for drawing on the window
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if(renderer == NULL){
+		cout << "Could not created renderer" << SDL_GetError() << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if(texture == NULL){
+		cout << "Could not created texture" << SDL_GetError() << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	// create the buffer that will be needed to save pixel information
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+	// set all pixels on the screen to white
+	memset(buffer, 0xFF, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
 	bool quit = false;
 
 	SDL_Event event;
 
-	while(!quit){
+	while (!quit) {
 		// Update particles
 		// Draw particles
 		// Check for messages/events
 
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL_QUIT){
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
 	}
 
+	delete [] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
